d04edc77780d13ccf4d160fa01fce9a158f10f69
Richard Klancer <rpk@pobox.com>
2012-10-13 12:19:19 -0400

    Motor class for use with MDC2200

--- rascal/server.py
+++ rascal/server.py
@@ -1,3 +1,46 @@
+class Motor:
+  SERIAL_SPEED = 115200
+  TIMEOUT_INTERVAL = 3
+
+  def __init__(self):
+    import time
+    self.last_command_string = ""
+    self.last_command_time = time.time()
+    self.timed_out = False
+
+
+  def send_command_string(self, command_string):
+    # import pytronics
+    
+    self.last_command_string = command_string
+    #pytronics.serialWrite(command_string, self.__class__.SERIAL_SPEED)
+    print command_string
+
+
+  def repeat_last_command_or_time_out(self):
+    import time
+
+    now = time.time()
+    if (now - self.last_command_time) > self.__class__.TIMEOUT_INTERVAL:
+      self.timed_out = True
+      return
+
+    self.send_command_string(self.last_command_string)     
+
+
+  def send_command(self, left, right):
+    import time
+
+    if (abs(left) > 1000) or (abs(right) > 1000):
+      raise ValueError("Motor command out of range")
+
+    self.last_command_time = time.time()
+    self.timed_out = False
+
+    command_string = "!M %d %d\r" % (left, right)
+    self.send_command_string(command_string)
+
+
 def interpret_joystick_command(x, y):
   """Accepts a joystick position (x, y) where x and y represent a point within the unit circle.
      Returns a tuple (left, right) of the corresponding motor speeds for a left and right motor in
